fa8112866869dc9de7525e978f358c68ba2b5c68
Noreddine Belhadj Cheikh <anouar1991belhadj@gmail.com>
2019-08-31 19:46:52 +0100

    [test3]: - Created function persistence.

--- functional.py
+++ functional.py
@@ -1,2 +1,17 @@
+import numpy as np
+
+
 def sum_int(lst):
     return sum([int(n) for n in lst if n.isdigit()])
+
+
+def rec_per(n, t):
+    str_n = str(n)
+    if len(str_n) == 1:
+        return t
+    mult = np.prod([int(n) for n in str_n])
+    return rec_per(mult, t+1)
+
+
+def persistence(n):
+    return rec_per(n, 0)
